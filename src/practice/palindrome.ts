05a3ee2728bf6e21ba44e13c3da994a133e68f99
MinhNguyen <minh.nguyen@kasha.io>
2025-10-24 23:12:57 +0700

    p1

--- src/practice/palindrome.ts
+++ src/practice/palindrome.ts
@@ -8,7 +8,24 @@
 // Input: "hello" → Output: false
 
 export default function isPalindrome(...args: any[]): any {
-  // TODO: Implement your solution here
-  console.log('Running isPalindrome with args:', args)
-  return null
+    // TODO: Implement your solution here
+    const srcString = args[0]
+    const length = srcString.length
+    let count = 0;
+    console.log('Running isPalindrome with args :=== >  ', srcString,length)
+    //  r a c e c a r
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = length - 1 - i; j >= 0; j--) {
+            //console.log('isPalindrome  ',i, ' --',j)
+            if (srcString[i] === srcString[j] && i < j) {
+                count++;
+                console.log('isPalindrome  ',i, ' --',j)
+            }
+        }
+    }
+    const checkingNum =  Math.floor(length / 2)
+
+    console.log('Running isPalindrome with args :=== >  ', checkingNum)
+
+    return checkingNum === count; 
 }
